423c702210cfe0de74bf595cef170fa527f7b7a3
romain tourrel <romaintourrel@ecole.ensicaen.fr>
2015-03-31 15:42:53 +0200

    Boucle de jeu RT v1.0

---
+++ SokobanGL/include/Case.hpp
@@ -0,0 +1,25 @@
+/**
+ * @title Matrice de caisses
+ * @author Romain Tourrel
+ */
+
+#ifndef CASE
+#define CASE
+
+#include <iostream>
+#include <math.h>
+#include <string.h>
+
+using namespace std;
+
+class Case {
+private:
+	boolean personnage;
+	boolean vide;
+	boolean caisse;
+	boolean cible;
+	boolean bord;
+
+};
+
+#endif
