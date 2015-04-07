7f8323774b5064df89476094ae79aaedd046a4ec
Virgile Trellu <virgile.trellu@ensicaen.fr>
2015-04-07 10:16:14 +0200

    matrice mise à jour

--- Modele/src/Matrix.cpp
+++ Modele/src/Matrix.cpp
@@ -8,7 +8,6 @@ Matrix Matrix::setMap(string chMatrix){
   bool emptyLine;
   if(fileMatrix){
     fileMatrix >> _column >> _row;
-    _row += 1;
     while(!fileMatrix.eof() && !emptyLine){
       getline(fileMatrix, line);
       cout << line.length() - 1 << endl;
@@ -16,8 +15,8 @@ Matrix Matrix::setMap(string chMatrix){
 	emptyLine = true;
       }else{
 	if(i == 0){
-	  _matrix = new Tile* [_row];
-	  for(int j = 0; j < _row; j++){
+	  _matrix = new Tile* [_row + 1];
+	  for(int j = 0; j < _row + 1; j++){
 	    _matrix[j] = new Tile[_column];
 	  }
 	}else{
